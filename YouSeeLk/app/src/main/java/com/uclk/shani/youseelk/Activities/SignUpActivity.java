c9ad06636d21028231608ad8f8cc965a7199c987
LENEX\naveen <naveen.cooray.nc@gmail.com>
2017-06-19 19:35:22 +0530

    added firebase authentication and realtime database support

--- YouSeeLk/app/src/main/java/com/uclk/shani/youseelk/Activities/SignUpActivity.java
+++ YouSeeLk/app/src/main/java/com/uclk/shani/youseelk/Activities/SignUpActivity.java
@@ -1,16 +1,35 @@
 package com.uclk.shani.youseelk.Activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.uclk.shani.youseelk.Objects.User;
 import com.uclk.shani.youseelk.R;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    DatabaseReference rootRef;
+    private EditText username;
+    private EditText password;
+    private EditText confirm;
+    private Button next;
+    private Button signUpFirst;
+    private Button hasAcc;
+    private FirebaseAuth mAuth;
+    //private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,26 +39,36 @@ public class SignUpActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
 
-        EditText username = (EditText)findViewById(R.id.etSUEmail);
-        EditText password = (EditText)findViewById(R.id.etSUPassword);
-        EditText confirm = (EditText)findViewById(R.id.etSUConfirmPassword);
-        Button next = (Button)findViewById(R.id.btnNextPref);
-        Button signupFirst = (Button)findViewById(R.id.btnSignUpWithoutPref);
-        Button hasAcc = (Button)findViewById(R.id.btnHasAccount);
+        mAuth = FirebaseAuth.getInstance();
+        username = (EditText)findViewById(R.id.etSUEmail);
+        password = (EditText)findViewById(R.id.etSUPassword);
+        confirm = (EditText)findViewById(R.id.etSUConfirmPassword);
+        next = (Button)findViewById(R.id.btnNextPref);
+        signUpFirst = (Button)findViewById(R.id.btnSignUpWithoutPref);
+        hasAcc = (Button)findViewById(R.id.btnHasAccount);
+        rootRef = FirebaseDatabase.getInstance().getReference();
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent1 = new Intent(getApplicationContext(),PreferencesActivity.class);
                 startActivity(intent1);
             }
         });
 
-        signupFirst.setOnClickListener(new View.OnClickListener() {
+        signUpFirst.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //code to save user details in DB
+                String uName = username.getText().toString().trim();
+                String pass = password.getText().toString().trim();
+                String conf =  confirm.getText().toString().trim();
+
+                if(comparePasswords(pass,conf)){
+                    createUser(uName,pass);
+                }else {
+                    Toast.makeText(getApplicationContext(),"Passwords do not match!", Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
@@ -55,5 +84,32 @@ public class SignUpActivity extends AppCompatActivity {
 
     }
 
+    boolean comparePasswords(String pass, String confirm){
+        //check if the entered passwords are matching then return
+        //simplified if else statement
+
+        return pass.equals(confirm);
+    }
+
+    void createUser(String username, String password){
+        //create a new user in the database under "user" child node
+
+        User user = new User(username,password);
+        rootRef.child("users").push().setValue(user);
+
+        mAuth.createUserWithEmailAndPassword(username,password).
+                addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(SignUpActivity.this,"New user authenticated!",
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(SignUpActivity.this,"New user authentication failed!",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
 
 }
---
+++ YouSeeLk/app/src/main/java/com/uclk/shani/youseelk/Objects/TourPlaces.java
@@ -0,0 +1,14 @@
+package com.uclk.shani.youseelk.Objects;
+
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.ArrayList;
+
+@IgnoreExtraProperties
+
+public class TourPlaces {
+
+    ArrayList<String> loactions = new ArrayList<>();
+
+
+}
